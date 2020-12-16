d5974f55702577aa0ed1854368103a0204d2084d
Marco Giulietti <ga6t00n@gmail.com>
2020-12-16 09:59:34 +0100

    Modificato il controllo sull'empty

--- index.php
+++ index.php
@@ -13,13 +13,15 @@
     $mail = $_GET['mail'];
     $age = $_GET['age'];
 
-    if (strlen($name) > 3) {
-        if  (strpos($mail, '@') && strpos($mail, '.') ) {
-            if (is_numeric($age)) {
-                $response = 'Accesso riuscito';
-            } else $response = 'Accesso non riuscito, età non valida';
-        } else $response = 'Accesso non riuscito, mail non valida';
-    } else $response = 'Accesso non riuscito, nome troppo breve';
+    if ( ! empty($name) && ! empty($mail) && ! empty($age) ) {
+        if (strlen($name) > 3) {
+            if  (strpos($mail, '@') && strpos($mail, '.') ) {
+                if (is_numeric($age)) {
+                    $response = 'Accesso riuscito';
+                } else $response = 'Accesso non riuscito, età non valida';
+            } else $response = 'Accesso non riuscito, mail non valida';
+        } else $response = 'Accesso non riuscito, nome troppo breve';
+    } else $response = 'Accesso non riuscito, mancano uno o tutti i parametri';
     // Querystring di prova: ?name=paolo&mail=paoloduzioni@hotmail.com&age=30
     ?>
 
